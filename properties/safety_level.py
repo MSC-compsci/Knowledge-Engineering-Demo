f5f920d82b10042e011b3c5106b8be2cee3f4ab1
miloszwatroba <milosz.watroba@gmail.com>
2019-12-14 21:18:38 +0100

    filtering added

--- properties/safety_level.py
+++ properties/safety_level.py
@@ -1,7 +1,15 @@
+from enum import Enum
+
 from properties.desired_property import DesiredProperty
 
 
+class SafetyValue(Enum):
+    ANY = 'any'
+    AVG = 'average'
+    SAFE = 'safest'
+
+
 class SafetyLevel(DesiredProperty):
-    def __init__(self, is_required: bool, min_level: str):
+    def __init__(self, is_required: bool, min_level: SafetyValue):
         super().__init__(is_required)
         self.min_level = min_level
